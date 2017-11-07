af4f67af059fd3002e3067d7642ce85575468128
dbsukkzy <szsutarat@gmail.com>
2017-11-07 13:46:32 +0700

    edit Scene

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,4 @@
-package sample;
+package mealbarn;
 
 public class Controller {
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,4 +1,4 @@
-package sample;
+package mealbarn;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -14,7 +14,7 @@ public class Main extends Application {
     double yOffset;
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("Login.fxml"));
         primaryStage.initStyle(StageStyle.TRANSPARENT);
         root.setOnMousePressed(new EventHandler<MouseEvent>() {
             @Override
