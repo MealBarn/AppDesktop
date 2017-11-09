7fc3d0f66e65226db0f2d6ef2e1251732eb10466
Mimobobo <panisa0605@gmail.com>
2017-11-09 15:41:05 +0700

    refactor code

--- OOAD/src/ooad/Main.java
+++ OOAD/src/ooad/Main.java
@@ -17,42 +17,71 @@ public class Main {
         // TODO code application logic here
         //Scanner input = new Scanner (System.in);
 
-
-        EntityManagerFactory emf = Persistence.createEntityManagerFactory("Account.odb");
+        String nameFile = "Food.odb";
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory(nameFile);
         EntityManager em = emf.createEntityManager();
-        
-        em.getTransaction().begin();        
-
-                    Account p = new Account();
-                    em.persist(p);
-                    
-                      p.setUsername("Suzy");
-                  
-                    p.setPassword("12345678");
-                   
-    
-                   
-//                    p.setName("ราดหน้าหมูหมัก");
-//                  
-//                    p.setType("a la carte");
-//                   
-//                    p.setRecipe("");
-                    
-        em.getTransaction().commit();
-//        TypedQuery<Food> query =
-//            em.createQuery("SELECT p FROM Food p", Food.class);
-//        List<Food> results = query.getResultList();
-//        for (Food p : results) {
-//            System.out.println(p);
-//        }
-//        Query query1 = em.createQuery("SELECT c FROM Food c WHERE c.id = 1");
-//        Food c = (Food)query.getSingleResult();
-//        System.out.print(c);
-
             
          em.close();
         emf.close();
          
+    }
+
+    /*For displaying list Food*/
+    public static void printAll(EntityManager em)
+    {
+              TypedQuery<Food> query = em.createQuery("SELECT p FROM Food p", Food.class);
+        List<Food> results = query.getResultList();
+        for (Food p : results) {
+            System.out.println(p);
+        }
+    }
+    
+    /*For displaying selected Food detail*/
+    public static void printSelect(EntityManager em)
+    {
+                Query query = em.createQuery("SELECT c FROM Food c WHERE c.id = 1");
+        Food c = (Food)query.getSingleResult();
+       System.out.print(c);
+    }
+    
+    /*For inserting object in objectDataBase Note!! obeject was identified identity number 
+    for example if you delect one object and create new one, ID was generated automatic.e.g. NO.35 was deleted,Next object will
+    be generated number is NO.36*/
+    public static void insertDBFood(EntityManager em,String nameFood,String nameType, String recipe)
+    {
+        /*Connect to database*/
+               em.getTransaction().begin();        
+
+                    Food p = new Food();
+                    em.persist(p);
+                    
+//                      p.setUsername("Suzy");
+                  
+//                    p.setPassword("12345678");           
+                    p.setName(nameFood);
+                    p.setType(nameType);                   
+                    p.setRecipe(recipe);
+                    
+        em.getTransaction().commit();
+        /*Close connect*/
+           
+
+    }
+    /*Same as insertDBFood*/
+    public static void insertDBAccount(EntityManager em,String username,String password)
+    {
+        /*Connect to database*/
+          em.getTransaction().begin();        
+
+                    Account p = new Account();
+                    em.persist(p);
+                    
+                    p.setUsername(username);
+                    p.setPassword(password);           
+ 
+                    
+        em.getTransaction().commit();
+        /*Close connect*/
     }
 }   
 
